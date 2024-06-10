63426dc379a9973f1ed4b51cf3b27fb52016dac0
UrielDelgado <delgadouriel83@gmail.com>
2024-06-10 10:25:44 -0700

    got it working
    everything works just need to do the second part

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const content = document.getElementById('content');
 
 document.addEventListener("DOMContentLoaded",function () {
      filterDropdown.addEventListener('change',getData)
+     categoryDropdown.addEventListener('change',() => { 
+        content.innerHTML = ""
+        const categoryValue = categoryDropdown.value;
+
+        fetch(`http://localhost:8089/api/products/bycategory/${categoryValue}`)
+        .then(response=> response.json())
+        .then(data => {showContent(data)});})
 
 });
 
@@ -15,24 +22,45 @@ function getData(){
 
     console.log(endpoint);
 
+    if(endpoint === `products`){
+        fetch(`http://localhost:8089/api/${endpoint}`)
+        .then(response=> response.json())
+        .then(data => {showContent(data)
 
+    })
+
+    } else if(endpoint === `categories`){
         fetch(`http://localhost:8089/api/${endpoint}`)
         .then(response=> response.json())
         .then(data => {GenerateDropdown(data)
 
-            
-
-})};
+})}};
 
 
 function GenerateDropdown(data) {
+    categoryDropdown.innerHTML = "";
     const fragment = document.createDocumentFragment();
+    
     data.forEach(item => {
         const option = document.createElement("option");
-        option.value = item.name;
+        option.value = item.categoryId;
         option.text = `${item.name} `;
         fragment.appendChild(option);
     });
     categoryDropdown.appendChild(fragment);
     console.log(data);
-};
+   
+}
+
+function showContent(products){
+    products.forEach(product => {
+        content.innerHTML += 
+        `
+         <div> ${product.productName}<div>
+        <div> ${product.unitPrice}<div>
+        <div> ${product.unitsInStock}<div>
+        `
+        
+    });
+
+}
